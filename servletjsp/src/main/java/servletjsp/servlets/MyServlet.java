10dca13cf24a2c98244c5c8544cb0af29d003a35
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-14 09:57:59 +0530

    Topic 3-l

--- servletjsp/src/main/java/servletjsp/servlets/MyServlet.java
+++ servletjsp/src/main/java/servletjsp/servlets/MyServlet.java
@@ -3,14 +3,16 @@ package servletjsp.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = "/myServlet")
 public class MyServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("<h1>This is my first servlet using Http Servlet.</h1>");
+		resp.getWriter().print("<h1>This is my first servlet using Http Servlet and @WebServlet.</h1>");
 	}
 }
