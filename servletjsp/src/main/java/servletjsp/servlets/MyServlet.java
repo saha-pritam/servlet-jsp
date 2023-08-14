8f8eeb156ccead54e0a18bd160dde596646649ed
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-14 09:47:12 +0530

    Topic 3-i

--- servletjsp/src/main/java/servletjsp/servlets/MyServlet.java
+++ servletjsp/src/main/java/servletjsp/servlets/MyServlet.java
@@ -2,45 +2,15 @@ package servletjsp.servlets;
 
 import java.io.IOException;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
+import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-public class MyServlet implements Servlet{
-	
-	
-	public MyServlet() {
-		System.out.println("Hi I am constructor.");
-	}
-
-	@Override
-	public void destroy() {
-		System.out.println("Hi I am destroy().");
-	}
-
-	@Override
-	public ServletConfig getServletConfig() {
-		System.out.println("Hi I am getServletConfig().");
-		return null;
-	}
-
-	@Override
-	public String getServletInfo() {
-		System.out.println("Hi I am getServletInfo().");
-		return null;
-	}
-
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		System.out.println("Hi I am init().");
-		
-	}
+public class MyServlet extends GenericServlet{
 
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-		res.getWriter().print("<h1>This is my first servlet.</h1>");
-		System.out.println("Hi I am service().");
+		res.getWriter().print("<h1>Hi this is my first servlet using generic class.");
 	}
 }
