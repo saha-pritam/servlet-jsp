e8066290f3771d7397178b6fb98b49a1b999399b
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-14 09:28:41 +0530

    Topic 3-d

---
+++ servletjsp/src/main/java/servletjsp/servlets/MyServlet.java
@@ -0,0 +1,40 @@
+package servletjsp.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyServlet implements Servlet{
+	
+	@Override
+	public void destroy() {
+		System.out.println("Hi I am destroy().");
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		System.out.println("Hi I am getServletConfig().");
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		System.out.println("Hi I am getServletInfo().");
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Hi I am init().");
+		
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		System.out.println("Hi I am service().");
+	}
+}
