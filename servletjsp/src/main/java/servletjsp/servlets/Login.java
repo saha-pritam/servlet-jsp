2cb5d648caa7ec855e130533df48c2e80772a15b
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-16 09:50:33 +0530

    Topic-10-f

--- servletjsp/src/main/java/servletjsp/servlets/Login.java
+++ servletjsp/src/main/java/servletjsp/servlets/Login.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +14,8 @@ public class Login extends HttpServlet{
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		Cookie cookie = new Cookie("user_email", req.getParameter("user_email"));
+		resp.addCookie(cookie);
 		resp.getWriter().print("<h1>Hi "+req.getParameter("user_email")+" you have successfully logged in.</h1>");
 		resp.getWriter().print("<a href=\"servlet\">Go To Next Page</a>");
 	}
--- servletjsp/src/main/java/servletjsp/servlets/Servlet.java
+++ servletjsp/src/main/java/servletjsp/servlets/Servlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,13 @@ public class Servlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("<h1>Hi "+req.getParameter("user_email")+" this is next page.</h1>");
+		Cookie[] cookies = req.getCookies();
+		String user_email=null;
+		for(Cookie cookie:cookies)
+			if(cookie.getName().equals("user_email")) {
+				user_email=cookie.getValue();
+				break;
+			}
+		resp.getWriter().print("<h1>Hi "+user_email+" this is next page.</h1>");
 	}
 }
