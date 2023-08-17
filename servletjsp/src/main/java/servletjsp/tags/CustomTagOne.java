7dfc17e8734953019c1c65cbe9490fca3fe9e0e2
Pritam Saha <saha.pritam.work@gmail.com>
2023-08-17 17:27:40 +0530

    Topic-15-e, 15-f-i, 15-f-iii

---
+++ servletjsp/src/main/java/servletjsp/tags/CustomTagOne.java
@@ -0,0 +1,44 @@
+package servletjsp.tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class CustomTagOne extends TagSupport {
+
+	@Override
+	public int doAfterBody() throws JspException {
+		JspWriter out = pageContext.getOut();
+		try {
+			out.print("<h4>I am doAfterBody() method </h4>");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return EVAL_PAGE;
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+		JspWriter out = pageContext.getOut();
+		try {
+			out.print("<h4>I am doEndTag() method </h4>");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return EVAL_PAGE;
+	}
+
+	@Override
+	public int doStartTag() throws JspException {
+		JspWriter out = pageContext.getOut();
+		try {
+			out.print("<h4>I am doStartTag() method </h4>");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return EVAL_BODY_INCLUDE;
+	}
+
+}
